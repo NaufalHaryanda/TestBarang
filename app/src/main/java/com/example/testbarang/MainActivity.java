3d6718d1e200347013e298130387b6f4a69660a7
NaufalHaryanda <naufalharyanda10@gmail.com>
2020-06-08 15:23:29 +0700

    Firebase2

--- app/src/main/java/com/example/testbarang/MainActivity.java
+++ app/src/main/java/com/example/testbarang/MainActivity.java
@@ -27,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
         bLihat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View V) {
-
             }
         });
 
